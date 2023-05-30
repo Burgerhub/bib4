408654fc3f7794f185b4980d49ff7773a38fc6d9
ruslanGIT <ruslan@gmail.com>
2023-05-30 23:56:29 +0300

    commit

---
+++ ClassLibrary1/Varian_24.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   public class Varian_24
+    {
+        public static int Task1(int[] x)
+        {
+            int result = 1;
+            int count = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (true)
+                {
+                    result *= i;
+                    count++;
+                }
+            }
+            return result;
+        }
+
+        public static int [] Task2 (int [] x1)
+        {
+            int[] result = new int[x1.Length];
+            for (int i = 0; i < x1.Length; i++)
+            {
+               result[i] = 6 * x1[i] - 7;
+            }
+
+            return result;
+        }
+
+        public static double[] Task3 (double[] x , double []x1)
+        {
+            double sum = 0;
+            double count = 0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] < 0)
+                {
+                    sum += x[i];
+                    count++;
+                }
+            }
+            for (int i = 0; i < x1.Length; i++)
+            {
+                if (x1[i] < 0)
+                {
+                    sum += x1[i];
+                    count++;
+                }
+            }
+            return sum;
+        }
+    } 
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] a = { 2, 4, 6};
+
+            int expecred = 15;
+
+            int actual = Varian_24.Task1(a);
+            Assert.AreEqual(expecred ,actual );
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] a = { 2, 4, 6};
+
+            int[] expecred = { 5, 17, 29 };
+
+            int[] actual = Varian_24.Task2(a);
+            CollectionAssert.AreEqual(expecred, actual);   
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+        }
+    }
+}
